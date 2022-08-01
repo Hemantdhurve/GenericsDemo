7b0f92c256edc8690eae79e990daac4e61c38de3
Hemant <hemantdhurve007@gmail.com>
2022-08-01 17:52:18 +0530

    Added GenericsDemo file

---
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericsDemo;
+using static GenericsDemo.SampleDemo;
+
+int[]
+    intArray = { 1, 2, 3, 4, 5 };
+double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+//Program.toPrint(intArray);
+//Program.toPrint(doubleArray);
+//Program.toPrint(charArray);
+
+SampleDemo.toPrint<int>(intArray);
+SampleDemo.toPrint<double>(doubleArray);
+SampleDemo.toPrint<char>(charArray);
---
+++ GenericsDemo/GenericsDemo/SampleDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class SampleDemo
+        {
+            public static void toPrint<T>(T[] inputArray)
+            {
+                foreach (var element in inputArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("----------------------------------");
+            }
+
+            public static void toPrint(int[] inputArray)
+            {
+                foreach (int element in inputArray) // 1, 2, 3, 4, 5
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("----------------------------------");
+            }
+
+            public static void toPrint(double[] inputArray)
+            {
+
+                foreach (double element in inputArray)//1.1, 2.2, 3.3, 4.4
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("----------------------------------");
+            }
+
+            public static void toPrint(char[] inputArray)
+            {
+                foreach (char element in inputArray) //'H', 'E', 'L', 'L', 'O'
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("-------------------------------------");
+            }
+
+        }
+    }
